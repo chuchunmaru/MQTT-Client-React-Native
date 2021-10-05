cb1340635ca4feb7375096164145859e556d1be4
chuchunmaru <giovannyariel2103@gmail.com>
2021-10-05 17:18:22 -0300

    Send Message Success

--- App.js
+++ App.js
@@ -2,38 +2,39 @@ import React from 'react'
 import { View, Text, TouchableOpacity, TextInput, StatusBar, ScrollView } from 'react-native'
 import MQTT from 'react-native-mqtt-angelos3lex'
 
-const sendMessage = () => {
+
+const sendMessage = (messageForSend) => {
+ 
+  MQTT.createClient({
+    uri: 'mqtt://test.mosquitto.org:1883',
+    clientId: 'chuchunmaru:)'
+  }).then(function(client) {
   
+    client.on('closed', function() {
+      console.log('mqtt.event.closed');
+    });
+  
+    client.on('error', function(msg) {
+      console.log('mqtt.event.error', msg);
+    });
+  
+    client.on('message', function(msg) {
+      
+      console.log(msg)
+      
+    });
+  
+    client.on('connect', function() {
+      console.log('connected');
+      client.subscribe('chuchunmaru', 0);
+      client.publish('chuchunmaru', messageForSend, 0, false); 
+    });
+  
+    client.connect();
+  }).catch(function(err){
+    console.log(err);
+  }); 
 }
-
-MQTT.createClient({
-  uri: 'mqtt://test.mosquitto.org:1883',
-  clientId: 'chuchunmaru:)'
-}).then(function(client) {
-
-  client.on('closed', function() {
-    console.log('mqtt.event.closed');
-  });
-
-  client.on('error', function(msg) {
-    console.log('mqtt.event.error', msg);
-  });
-
-  client.on('message', function(msg) {
-    console.log('mqtt.event.message', msg);
-    
-  });
-
-  client.on('connect', function() {
-    console.log('connected');
-    client.subscribe('chuchunmaru', 0);
-    client.publish('chuchunmaru', "teste message", 0, false);
-  });
-
-  client.connect();
-}).catch(function(err){
-  console.log(err);
-});
 
 export default class App extends React.Component {
 
@@ -44,7 +45,7 @@ export default class App extends React.Component {
         <View style={{backgroundColor: 'black', height:'100%', width:'100%', justifyContent: 'center', alignItems:'center'}}>
           <StatusBar hidden />
           <Text style={{fontSize:30}}>MQTT CLIENT TESTS</Text>
-          <TouchableOpacity onPress={() => sendMessage()} style={{marginVertical:40, marginHorizontal:20, borderRadius:8, height: 50, width: '98%', backgroundColor:'#262626', borderWidth:2, borderColor:'rgba(255, 255, 255, .4)', justifyContent:"center"}}>
+          <TouchableOpacity onPress={() => sendMessage("Mob::giovannyariel2103@gmail.com::5518991893276")} style={{marginVertical:40, marginHorizontal:20, borderRadius:8, height: 50, width: '98%', backgroundColor:'#262626', borderWidth:2, borderColor:'rgba(255, 255, 255, .4)', justifyContent:"center"}}>
             <View>
               <Text style={{fontSize:25,  textAlign:'center', color:'rgba(255,255,255,.5)'}}>
                 SEND MESSAGE FOR ESP
