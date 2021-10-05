67a88dbdd2fdfcdf72ac51cd979357a3164cf1dc
chuchunmaru <giovannyariel2103@gmail.com>
2021-10-05 18:31:36 -0300

    Get Esp IPAddress

--- App.js
+++ App.js
@@ -4,36 +4,41 @@ import MQTT from 'react-native-mqtt-angelos3lex'
 
 
 const sendMessage = (messageForSend) => {
- 
+
   MQTT.createClient({
     uri: 'mqtt://test.mosquitto.org:1883',
     clientId: 'chuchunmaru:)'
-  }).then(function(client) {
-  
-    client.on('closed', function() {
+  }).then(function (client) {
+
+    client.on('closed', function () {
       console.log('mqtt.event.closed');
     });
-  
-    client.on('error', function(msg) {
+
+    client.on('error', function (msg) {
       console.log('mqtt.event.error', msg);
     });
-  
-    client.on('message', function(msg) {
-      
+
+    client.on('message', function (msg) {
+
       console.log(msg)
-      
+
+      if (msg.data.match(/EspUrlToConfig/)) {
+        var ipadress = msg.data.replace(/EspUrlToConfig/i, '')
+        console.log(ipadress)
+      }
+
     });
-  
-    client.on('connect', function() {
+
+    client.on('connect', function () {
       console.log('connected');
       client.subscribe('chuchunmaru', 0);
-      client.publish('chuchunmaru', messageForSend, 0, false); 
+      client.publish('chuchunmaru', messageForSend, 0, false);
     });
-  
+
     client.connect();
-  }).catch(function(err){
+  }).catch(function (err) {
     console.log(err);
-  }); 
+  });
 }
 
 export default class App extends React.Component {
@@ -41,18 +46,18 @@ export default class App extends React.Component {
 
   render() {
 
-      return (
-        <View style={{backgroundColor: 'black', height:'100%', width:'100%', justifyContent: 'center', alignItems:'center'}}>
-          <StatusBar hidden />
-          <Text style={{fontSize:30}}>MQTT CLIENT TESTS</Text>
-          <TouchableOpacity onPress={() => sendMessage("Mob::giovannyariel2103@gmail.com::5518991893276")} style={{marginVertical:40, marginHorizontal:20, borderRadius:8, height: 50, width: '98%', backgroundColor:'#262626', borderWidth:2, borderColor:'rgba(255, 255, 255, .4)', justifyContent:"center"}}>
-            <View>
-              <Text style={{fontSize:25,  textAlign:'center', color:'rgba(255,255,255,.5)'}}>
-                SEND MESSAGE FOR ESP
-              </Text>
-            </View>
-          </TouchableOpacity>
-        </View>
-      )
-}
+    return (
+      <View style={{ backgroundColor: 'black', height: '100%', width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <StatusBar hidden />
+        <Text style={{ fontSize: 30 }}>MQTT CLIENT TESTS</Text>
+        <TouchableOpacity onPress={() => sendMessage("RequestUrlConfig")} style={{ marginVertical: 40, marginHorizontal: 20, borderRadius: 8, height: 50, width: '98%', backgroundColor: '#262626', borderWidth: 2, borderColor: 'rgba(255, 255, 255, .4)', justifyContent: "center" }}>
+          <View>
+            <Text style={{ fontSize: 25, textAlign: 'center', color: 'rgba(255,255,255,.5)' }}>
+              SEND MESSAGE FOR ESP
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    )
+  }
 }
